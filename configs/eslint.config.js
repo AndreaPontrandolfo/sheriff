73ee29b0f584c799adb8be83dceccfebff14428f
AndreaPontrandolfo <andrea.pontra@gmail.com>
2023-01-28 23:56:12 +0100

    feat(rules): added more playwright eslint rules

--- configs/eslint.config.js
+++ configs/eslint.config.js
@@ -374,6 +374,8 @@ const playwrightHandPickedRules = {
   'playwright/prefer-lowercase-title': 2,
   'playwright/prefer-to-have-length': 2,
   'playwright/require-top-level-describe': 2,
+  'playwright/prefer-to-be': 2,
+  'playwright/prefer-strict-equal': 2,
 };
 
 const lodashHandPickedRules = {
