c5503d676670e63d2cf946168f1160545f349b75
AndreaPontrandolfo <andrea.pontra@gmail.com>
2023-07-10 01:17:06 +0200

    feat(typescript): updated deps, added TS declaration file export

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,35 @@
 interface SheriffOptions {
+  /**
+   * react support
+   */
   react?: boolean;
+  /**
+   * lodash support
+   */
   lodash?: boolean;
+  /**
+   * next support
+   */
   next?: boolean;
+  /**
+   * playwright support
+   */
   playwright?: boolean;
+  /**
+   * jest support
+   */
   jest?: boolean;
+  /**
+   * vitest support. Uses eslint-plugin-jest
+   */
   vitest?: boolean;
+  /**
+   * If you have multiple tsconfig.json files in your project (like tsconfig.json, tsconfig.eslint.json, tsconfig.node.json, etc...) you can specify which config Sheriff will pickup with the customTSConfigPath option
+   */
   customTSConfigPath?: string;
+  /**
+   * this setting accepts an array of filepaths, dictaced by minimatch syntax. Only the matching files found in this array will be linted
+   */
   files?: string[];
 }
 
