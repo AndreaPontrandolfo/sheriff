38c8f4cfc7619cf6a916496b2047f33971c98c18
AndreaPontrandolfo <andrea.pontra@gmail.com>
2023-08-10 01:43:32 +0200

    adjusted theme colormode

--- apps/docs-website/docusaurus.config.js
+++ apps/docs-website/docusaurus.config.js
@@ -61,6 +61,9 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
       navbar: {
         title: 'Sheriff',
         logo: {
