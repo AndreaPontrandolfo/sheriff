1652be1a831c38b4d1d8d70c8b7cd573871dc342
AndreaPontrandolfo <andrea.pontra@gmail.com>
2023-08-11 00:20:26 +0200

    implmented clean and publint

--- apps/docs-website/docusaurus.config.js
+++ apps/docs-website/docusaurus.config.js
@@ -92,8 +92,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Docs',
+                to: '/docs/introduction',
               },
             ],
           },
