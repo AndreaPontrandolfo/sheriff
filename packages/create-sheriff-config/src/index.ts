0cb244a6220eb303b41f553cfd1ed281ef35da61
AndreaPontrandolfo <andrea.pontra@gmail.com>
2023-12-28 17:20:53 +0100

    feat(types): made publishable @sheriff/types

--- packages/create-sheriff-config/src/index.ts
+++ packages/create-sheriff-config/src/index.ts
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
 import yargs, { type Arguments } from 'yargs';
-import { setSheriffConfig } from './src/utils/setSheriffConfig';
-import { setDependencies } from './src/utils/setDependencies';
-import { setEslintConfig } from './src/utils/setEslintConfig';
-import { setPrettierConfig } from './src/utils/setPrettierConfig';
-import { setPrettierIgnore } from './src/utils/setPrettierIgnore';
-import { askForCustomPath } from './src/utils/askForCustomPath';
-import { askForPrettierSupport } from './src/utils/askForPrettierSupport';
+import { setDependencies } from './utils/setDependencies';
+import { setEslintConfig } from './utils/setEslintConfig';
+import { setPrettierConfig } from './utils/setPrettierConfig';
+import { setPrettierIgnore } from './utils/setPrettierIgnore';
+import { askForCustomPath } from './utils/askForCustomPath';
+import { askForPrettierSupport } from './utils/askForPrettierSupport';
+import { askForEslintTsPatch } from './utils/askForEslintTsPatch';
+
 // import { Command, program } from 'commander';
 // import { widgets } from './src/utils/widgets';
 // import { spinnerError, stopSpinner } from "./src/utils/spinner";
@@ -28,6 +29,8 @@ async function main() {
     await askForCustomPath();
     await askForPrettierSupport();
   }
+
+  const isEslintTsPatchRequired = await askForEslintTsPatch();
 
   await setEslintConfig();
   if (!isSubProject || global.hasLocalPrettierSupport) {
---
+++ packages/create-sheriff-config/src/utils/askForEslintTsPatch.ts
@@ -0,0 +1,25 @@
+import promptShape from 'prompts';
+import { isBoolean } from 'lodash';
+import { logger } from './logs';
+
+export const askForEslintTsPatch = async (): Promise<boolean> => {
+  logger.verbose(`Do you want to use the eslint-ts-patch?`);
+
+  const response = await promptShape({
+    type: 'confirm',
+    name: 'eslintTsPatchConfirmValue',
+    message: 'eslint-ts-patch support (Experimental. Not recommended.)',
+  });
+
+  const { eslintTsPatchConfirmValue } = response;
+
+  if (isBoolean(eslintTsPatchConfirmValue)) {
+    return eslintTsPatchConfirmValue;
+  }
+
+  logger.error(
+    `Invalid response. Expected boolean, received ${eslintTsPatchConfirmValue}`,
+  );
+
+  return false;
+};
