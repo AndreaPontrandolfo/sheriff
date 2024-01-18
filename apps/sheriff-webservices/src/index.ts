12c4ffaa90d9208c8e6d6cc831228b66328c7c14
AndreaPontrandolfo <andrea.pontra@gmail.com>
2024-01-18 23:01:24 +0100

    feat(webservices): reimplemented backend with hono

--- apps/sheriff-webservices/src/index.ts
+++ apps/sheriff-webservices/src/index.ts
@@ -1,21 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import getSheriffConfig from "eslint-config-sheriff";
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { cors } from "hono/cors";
 import type { BarebonesConfigAtom } from "@sherifforg/types";
-import express, { type Request, type Response } from "express";
-import cors from "cors";
 import { generateRulesDataset } from "./generateRulesDataset.js";
 
-const app = express();
-const port = process.env.PORT || 5001;
+const app = new Hono();
+const port = Number(process.env.PORT || 5001);
 
-app.use(express.json());
-app.use(cors());
+app.use("/api/*", cors());
 
-app.get("/api/keepalive", (req: Request, res: Response) => {
-  res.send("OK");
-});
-app.post("/api/get-new-sheriff-config", (req: Request, res: Response) => {
-  const newConfig: BarebonesConfigAtom[] = getSheriffConfig(req.body);
+app.get("/api/keepalive", (context) => context.text("OK"));
+app.post("/api/get-new-sheriff-config", (context) => {
+  const newConfig: BarebonesConfigAtom[] = getSheriffConfig(context.req.json());
   const allRulesConfig: BarebonesConfigAtom[] = getSheriffConfig(
     {
       react: true,
@@ -35,13 +33,19 @@ app.post("/api/get-new-sheriff-config", (req: Request, res: Response) => {
 
   console.info("New config sent.");
 
-  res.send({
+  return context.json({
     compiledConfig,
     pluginsNames,
     totalAvailableRulesAmount: allRulesCompiledConfig.length,
   });
 });
 
-app.listen(port, () => {
-  console.debug(`Server is running on port ${port}`);
-});
+serve(
+  {
+    fetch: app.fetch,
+    port,
+  },
+  (info) => {
+    console.debug(`Server is running on port ${info.port}`);
+  },
+);
