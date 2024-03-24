aa7501aae41e4f828b3646d77f5d93edd4ebd064
AndreaPontrandolfo <andrea.pontra@gmail.com>
2024-03-24 01:02:42 +0100

    fix(rules): ignored use-unknown-in-catch-callback-variable

--- packages/eslint-config-sheriff/src/getExportableConfig.ts
+++ packages/eslint-config-sheriff/src/getExportableConfig.ts
@@ -158,7 +158,7 @@ const getBaseConfig = (userConfigChoices: SheriffSettings) => {
     },
     {
       files: [supportedFileTypes],
-      extends: [tseslint.configs.strictTypeChecked],
+      extends: tseslint.configs.strictTypeChecked,
     },
     {
       files: [supportedFileTypes],
