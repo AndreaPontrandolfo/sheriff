d24deffbc36e45b31d2fc2324ebfd73371979866
AndreaPontrandolfo <andrea.pontra@gmail.com>
2024-04-29 00:10:49 +0200

    feat(cli): patched nypm

--- packages/create-sheriff-config/src/utils/askForEslintTsPatch.ts
+++ packages/create-sheriff-config/src/utils/askForEslintTsPatch.ts
@@ -3,7 +3,7 @@ import { colors } from 'consola/utils';
 
 export const askForEslintTsPatch = async (): Promise<boolean> => {
   const isTsEslintPatchWanted = await consola.prompt(
-    `Do you want to use the ${colors.bold('eslint-ts-patch')}? ${colors.bgYellow('(Experimental)')}`,
+    `Do you want to use the ${colors.bold('eslint-ts-patch')}?`,
     {
       type: 'confirm',
       // onState: gracefullyAbort,
