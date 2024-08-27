c98373dc069c5fdb8b3f8cbc6470ba061ef4aa2e
AndreaPontrandolfo <andrea.pontra@gmail.com>
2024-08-28 01:52:58 +0200

    feat[cli]: Add prompt graceful cancellation Fixes #241

--- packages/sheriff-create-config/src/utils/askForPrettierSupport.ts
+++ packages/sheriff-create-config/src/utils/askForPrettierSupport.ts
@@ -1,7 +1,7 @@
-import { consola } from 'consola';
+import { customConsolaPrompt } from './customConsolaPrompt';
 
 export const askForPrettierSupport = async (): Promise<boolean> => {
-  const isPrettierWanted = await consola.prompt(
+  const isPrettierWanted = await customConsolaPrompt(
     `Do you want to add Prettier support in the workspace?
 Tip: if you want to use Prettier from the root of the monorepo, choose 'No'.`,
     {
