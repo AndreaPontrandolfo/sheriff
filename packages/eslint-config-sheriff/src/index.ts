0ed0ec6a59559a63cd107064d352ea0cce50db3f
AndreaPontrandolfo <andrea.pontra@gmail.com>
2024-10-22 00:19:00 +0200

    added some comments

--- packages/eslint-config-sheriff/src/index.ts
+++ packages/eslint-config-sheriff/src/index.ts
@@ -9,6 +9,7 @@ import {
 } from '@sherifforg/constants';
 import { getIndexedBaseNoRestrictedSyntaxRules } from './utils/getIndexedBaseNoRestrictedSyntaxRules';
 
+//#region - these '@sherifforg/constants' variables needs to be re-stored like this, otherwise they will not be bundled. They need to be bundled because we are not publishing the 'sheriff-constants' package, but we are using it as a dependency in this package.
 const exportableAllJsExtensions = allJsExtensions;
 const exportableAllJsxExtensions = allJsxExtensions;
 const exportableIgnores = ignores;
@@ -18,6 +19,7 @@ const exportableTestsFilePatterns = testsFilePatterns;
 
 const indexedBaseNoRestrictedSyntaxRules =
   getIndexedBaseNoRestrictedSyntaxRules();
+//#endregion
 
 export {
   getExportableConfig as sheriff,
