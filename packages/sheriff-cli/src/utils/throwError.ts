9fbd7c5803c866a4404dcd6651b29daff9c7a79f
AndreaPontrandolfo <andrea.pontra@gmail.com>
2024-12-28 12:10:08 +0100

    fixed(cli) aligned throwError util

--- packages/sheriff-cli/src/utils/throwError.ts
+++ packages/sheriff-cli/src/utils/throwError.ts
@@ -4,6 +4,15 @@ interface Error {
   error?: unknown;
 }
 
+/**
+ *
+ * Log an error message with stack trace and cause, then exits the process.
+ *
+ * // TODO: move this to a "sheriff-utils" package.
+ *
+ * @param message - The error message to display.
+ * @param error - The error object to display as the cause of the error.
+ */
 export const throwError = (message: string, { error }: Error = {}): void => {
   consola.error(new Error(message, error ? { cause: error } : undefined));
   process.exit(1);
--- packages/sheriff-create-config/src/utils/throwError.ts
+++ packages/sheriff-create-config/src/utils/throwError.ts
@@ -1,15 +1,19 @@
 import { consola } from 'consola';
-import { isString } from 'lodash-es';
 
 interface Error {
   error?: unknown;
 }
 
+/**
+ *
+ * Log an error message with stack trace and cause, then exits the process.
+ *
+ * // TODO: move this to a "sheriff-utils" package.
+ *
+ * @param message - The error message to display.
+ * @param error - The error object to display as the cause of the error.
+ */
 export const throwError = (message: string, { error }: Error = {}): void => {
-  if (error) {
-    consola.error(message);
-    throw new Error(isString(error) ? error : JSON.stringify(error));
-  }
-
-  throw new Error(message);
+  consola.error(new Error(message, error ? { cause: error } : undefined));
+  process.exit(1);
 };
