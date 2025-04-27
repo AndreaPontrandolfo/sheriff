bc050dc3878ac62724866e8c2da2cfb56454e88d
AndreaPontrandolfo <andrea.pontra@gmail.com>
2025-04-28 00:02:08 +0200

    feat: update global styles and layout for shadcn compatibility

--- apps/docs-website-fumadocs/src/app/(home)/page.tsx
+++ apps/docs-website-fumadocs/src/app/(home)/page.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link';
 
 export default function HomePage() {
+  // bg-background and text-foreground were added here because of shadcn compatibility. Reference: https://github.com/fuma-nama/fumadocs-shadcn?tab=readme-ov-file#with-shadcn-ui
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
+    <main className="bg-background text-foreground flex flex-1 flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
       <p className="text-fd-muted-foreground">
         You can open{' '}
