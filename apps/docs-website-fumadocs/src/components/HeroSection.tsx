e20146b95901bb67f4b3c6fa50a4dd03bb33370f
AndreaPontrandolfo <andrea.pontra@gmail.com>
2025-05-11 23:07:06 +0200

    cleanup hero

--- apps/docs-website-fumadocs/src/components/HeroSection.tsx
+++ apps/docs-website-fumadocs/src/components/HeroSection.tsx
@@ -26,87 +26,70 @@ const transitionVariants = {
 
 export function HeroSection() {
   return (
-    <>
-      <main className="overflow-hidden">
-        <div
-          aria-hidden
-          className="absolute inset-0 isolate hidden contain-strict lg:block"
-        >
-          <div className="w-140 h-320 -translate-y-87.5 absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
-          <div className="h-320 absolute left-0 top-0 w-60 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
-          <div className="h-320 -translate-y-87.5 absolute left-0 top-0 w-60 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
-        </div>
-        <section>
-          <div className="relative pt-24">
-            <div className="absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--color-background)_75%)]"></div>
-            <div className="mx-auto max-w-5xl px-6">
-              <div className="sm:mx-auto lg:mr-auto lg:mt-0">
-                <TextEffect
-                  preset="fade-in-blur"
-                  speedSegment={0.3}
-                  as="h1"
-                  className="mt-8 max-w-2xl text-balance text-5xl font-medium md:text-6xl lg:mt-16"
-                >
-                  Build and Ship 10x faster with NS
-                </TextEffect>
-                <TextEffect
-                  per="line"
-                  preset="fade-in-blur"
-                  speedSegment={0.3}
-                  delay={0.5}
-                  as="p"
-                  className="mt-8 max-w-2xl text-pretty text-lg dark:text-zinc-400"
-                >
-                  Tailwindcss highly customizable components for building modern
-                  websites and applications that look and feel the way you mean
-                  it.
-                </TextEffect>
+    <section>
+      <div className="relative pt-24">
+        <div className="absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--color-background)_75%)]"></div>
+        <div className="mx-auto max-w-5xl px-6">
+          <div className="sm:mx-auto lg:mr-auto lg:mt-0">
+            <TextEffect
+              preset="fade-in-blur"
+              speedSegment={0.3}
+              as="h1"
+              className="mt-8 max-w-2xl text-balance text-5xl font-medium md:text-6xl lg:mt-16"
+            >
+              Build and Ship 10x faster with NS
+            </TextEffect>
+            <TextEffect
+              per="line"
+              preset="fade-in-blur"
+              speedSegment={0.3}
+              delay={0.5}
+              as="p"
+              className="mt-8 max-w-2xl text-pretty text-lg dark:text-zinc-400"
+            >
+              Tailwindcss highly customizable components for building modern
+              websites and applications that look and feel the way you mean it.
+            </TextEffect>
 
-                <AnimatedGroup
-                  variants={{
-                    container: {
-                      visible: {
-                        transition: {
-                          staggerChildren: 0.05,
-                          delayChildren: 0.75,
-                        },
-                      },
+            <AnimatedGroup
+              variants={{
+                container: {
+                  visible: {
+                    transition: {
+                      staggerChildren: 0.05,
+                      delayChildren: 0.75,
                     },
-                    ...transitionVariants,
-                  }}
-                  className="mt-12 flex items-center gap-2"
-                >
-                  <div
-                    key={1}
-                    className="bg-foreground/10 rounded-[calc(var(--radius-xl)+0.125rem)] border p-0.5"
-                  >
-                    <Button
-                      asChild
-                      size="lg"
-                      className="rounded-xl px-5 text-base"
-                    >
-                      <Link href="#link">
-                        <span className="text-nowrap">Get started</span>
-                      </Link>
-                    </Button>
-                  </div>
-                  <Button
-                    key={2}
-                    asChild
-                    size="lg"
-                    variant="ghost"
-                    className="h-10.5 rounded-xl px-5 text-base"
-                  >
-                    <Link href="#link">
-                      <span className="text-nowrap">Rules</span>
-                    </Link>
-                  </Button>
-                </AnimatedGroup>
+                  },
+                },
+                ...transitionVariants,
+              }}
+              className="mt-12 flex items-center gap-2"
+            >
+              <div
+                key={1}
+                className="bg-foreground/10 rounded-[calc(var(--radius-xl)+0.125rem)] border p-0.5"
+              >
+                <Button asChild size="lg" className="rounded-xl px-5 text-base">
+                  <Link href="#link">
+                    <span className="text-nowrap">Get started</span>
+                  </Link>
+                </Button>
               </div>
-            </div>
+              <Button
+                key={2}
+                asChild
+                size="lg"
+                variant="ghost"
+                className="h-10.5 rounded-xl px-5 text-base"
+              >
+                <Link href="#link">
+                  <span className="text-nowrap">Rules</span>
+                </Link>
+              </Button>
+            </AnimatedGroup>
           </div>
-        </section>
-      </main>
-    </>
+        </div>
+      </div>
+    </section>
   );
 }
