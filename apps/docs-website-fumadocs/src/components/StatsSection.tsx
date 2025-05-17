5036b6d6f866a01fe489d019e778d1dd46784a8f
AndreaPontrandolfo <andrea.pontra@gmail.com>
2025-05-17 21:04:15 +0200

    Integrate AnimatedNumberInView component into StatsSection for dynamic number display, enhancing visual engagement and interactivity of statistics.

--- apps/docs-website-fumadocs/src/components/StatsSection.tsx
+++ apps/docs-website-fumadocs/src/components/StatsSection.tsx
@@ -1,6 +1,7 @@
 import { Pencil, Settings2, Sparkles } from 'lucide-react';
 import { HPSectionTitle } from './HPSectionTitle';
 import { HPSectionStart } from './HPSectionStart';
+import { AnimatedNumberInView } from './AnimatedNumberOnview';
 
 export const StatsSection = () => {
   return (
@@ -9,27 +10,49 @@ export const StatsSection = () => {
       <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
         <div className="relative mx-auto grid max-w-4xl divide-x divide-y border *:p-12 sm:grid-cols-2 lg:grid-cols-3">
           <div className="flex flex-col justify-center space-y-3 text-left">
-            <div className="text-5xl font-bold">10</div>
+            <div className="text-5xl font-bold">
+              <AnimatedNumberInView number={10} />
+            </div>
             <p>Technologies</p>
           </div>
           <div className="flex flex-col justify-center space-y-2 text-center">
-            <div className="text-5xl font-bold">27</div>
+            <div className="text-5xl font-bold">
+              <AnimatedNumberInView number={27} />
+            </div>
             <p>Plugins</p>
           </div>
           <div className="flex flex-col justify-center space-y-2 text-right">
-            <div className="text-5xl font-bold">+1332</div>
+            <div className="text-5xl font-bold">
+              +
+              <AnimatedNumberInView
+                number={1332}
+                durationInMilliseconds={1500}
+              />
+            </div>
             <p>Rules</p>
           </div>
           <div className="flex flex-col justify-center space-y-2 text-left">
-            <div className="text-5xl font-bold">+155</div>
+            <div className="text-5xl font-bold">
+              +
+              <AnimatedNumberInView
+                number={155}
+                durationInMilliseconds={1200}
+              />
+            </div>
             <p>Github stars</p>
           </div>
           <div className="flex flex-col justify-center space-y-2 text-center">
-            <div className="text-5xl font-bold">+70K</div>
+            <div className="text-5xl font-bold">
+              +
+              <AnimatedNumberInView number={70} durationInMilliseconds={2000} />
+              K
+            </div>
             <p>Monthly downloads</p>
           </div>
           <div className="flex flex-col justify-center space-y-2 text-right">
-            <div className="text-5xl font-bold">+10</div>
+            <div className="text-5xl font-bold">
+              +<AnimatedNumberInView number={10} />
+            </div>
             <p>Contributors</p>
           </div>
         </div>
