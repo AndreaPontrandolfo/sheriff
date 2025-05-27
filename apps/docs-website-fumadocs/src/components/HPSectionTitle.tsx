4af580ffe8c29f1669b9c1f8f8376249cd08e2c3
AndreaPontrandolfo <andrea.pontra@gmail.com>
2025-05-28 01:38:25 +0200

    Enhance HPSectionTitle component with gradient background for improved visual appeal and text styling.

--- apps/docs-website-fumadocs/src/components/HPSectionTitle.tsx
+++ apps/docs-website-fumadocs/src/components/HPSectionTitle.tsx
@@ -4,6 +4,8 @@ interface HPSectionTitleProps {
 
 export const HPSectionTitle = ({ title }: HPSectionTitleProps) => {
   return (
-    <h2 className="text-balance text-3xl font-semibold md:text-4xl">{title}</h2>
+    <h2 className="text-balance bg-gradient-to-t from-zinc-600 from-0% via-zinc-300 via-40% to-zinc-200 to-100% bg-clip-text text-3xl font-semibold text-transparent md:text-4xl">
+      {title}
+    </h2>
   );
 };
