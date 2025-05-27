352889c666e33a1b2544a12d9dbcb87109df5ae5
AndreaPontrandolfo <andrea.pontra@gmail.com>
2025-05-28 00:33:06 +0200

    Refactor StatsSection component to improve layout and typography. Adjusted max-width and font sizes for better responsiveness and readability.

--- apps/docs-website-fumadocs/src/components/StatsSection.tsx
+++ apps/docs-website-fumadocs/src/components/StatsSection.tsx
@@ -7,22 +7,22 @@ export const StatsSection = () => {
   return (
     <section className="py-20">
       <HPSectionStart title="Sheriff in numbers" />
-      <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
-        <div className="relative mx-auto grid max-w-4xl divide-x divide-y border *:p-12 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="mx-auto max-w-2xl space-y-8 px-6 md:max-w-5xl md:space-y-16">
+        <div className="relative mx-auto grid max-w-4xl grid-cols-3 divide-x divide-y border *:p-12">
           <div className="flex flex-col justify-center space-y-3 text-left">
-            <div className="text-5xl font-bold">
+            <div className="text-4xl font-bold md:text-5xl">
               <AnimatedNumberInView number={10} />
             </div>
             <p>Technologies</p>
           </div>
           <div className="flex flex-col justify-center space-y-2 text-center">
-            <div className="text-5xl font-bold">
+            <div className="text-4xl font-bold md:text-5xl">
               <AnimatedNumberInView number={27} />
             </div>
             <p>Plugins</p>
           </div>
           <div className="flex flex-col justify-center space-y-2 text-right">
-            <div className="text-5xl font-bold">
+            <div className="text-4xl font-bold md:text-5xl">
               +
               <AnimatedNumberInView
                 number={1332}
@@ -32,7 +32,7 @@ export const StatsSection = () => {
             <p>Rules</p>
           </div>
           <div className="flex flex-col justify-center space-y-2 text-left">
-            <div className="text-5xl font-bold">
+            <div className="text-4xl font-bold md:text-5xl">
               +
               <AnimatedNumberInView
                 number={155}
@@ -42,7 +42,7 @@ export const StatsSection = () => {
             <p>Github stars</p>
           </div>
           <div className="flex flex-col justify-center space-y-2 text-center">
-            <div className="text-5xl font-bold">
+            <div className="text-4xl font-bold md:text-5xl">
               +
               <AnimatedNumberInView number={70} durationInMilliseconds={2000} />
               K
@@ -50,7 +50,7 @@ export const StatsSection = () => {
             <p>Monthly downloads</p>
           </div>
           <div className="flex flex-col justify-center space-y-2 text-right">
-            <div className="text-5xl font-bold">
+            <div className="text-4xl font-bold md:text-5xl">
               +<AnimatedNumberInView number={10} />
             </div>
             <p>Contributors</p>
