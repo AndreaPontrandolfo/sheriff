9b52d2e87ab5b7cd33e64b54aa6dba15c317c241
AndreaPontrandolfo <andrea.pontra@gmail.com>
2025-06-20 01:54:27 +0200

    update layout behavior for homepage and non-homepage views

--- apps/docs-website/src/components/Footer.tsx
+++ apps/docs-website/src/components/Footer.tsx
@@ -1,16 +1,31 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
+
 function Footer() {
+  const pathname = usePathname();
+  const isHomepage = pathname === '/';
+
   return (
     <div className="relative">
       <footer
-        className="absolute bottom-0 left-0 right-0 flex flex-col border-b bg-white transition-[margin] md:[--fd-sidebar-width:268px] lg:[--fd-sidebar-width:286px] xl:[--fd-toc-width:286px] dark:bg-transparent"
+        className={cn(
+          'flex flex-col border-b bg-white dark:bg-transparent',
+          isHomepage
+            ? 'mx-auto max-w-4xl px-2 pt-20'
+            : 'absolute bottom-0 left-0 right-0 transition-[margin] md:[--fd-sidebar-width:268px] lg:[--fd-sidebar-width:286px] xl:[--fd-toc-width:286px]',
+        )}
         style={{
-          marginInlineStart: `var(--fd-sidebar-width)`,
+          marginInlineStart: isHomepage ? 'auto' : 'var(--fd-sidebar-width)',
         }}
       >
         <div
           className="mx-auto flex w-full flex-1"
           style={{
-            maxWidth: `min(var(--fd-page-width),calc(var(--fd-layout-width) - var(--fd-sidebar-width)))`,
+            maxWidth: isHomepage
+              ? 'auto'
+              : 'min(var(--fd-page-width),calc(var(--fd-layout-width) - var(--fd-sidebar-width)))',
           }}
         >
           <div className="mx-6 w-full border-t py-6 md:mx-auto md:w-[calc(100%_-_6rem)]">
