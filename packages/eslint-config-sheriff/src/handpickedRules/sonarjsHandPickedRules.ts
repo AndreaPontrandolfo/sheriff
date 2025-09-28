9dc6957cd6c62f742b3ebc4c47a5285f403fa1b2
AndreaPontrandolfo <andrea.pontra@gmail.com>
2025-09-28 22:25:56 +0200

    feat(config): removed sonarjs/no-duplicate-string rule

--- packages/eslint-config-sheriff/src/handpickedRules/sonarjsHandPickedRules.ts
+++ packages/eslint-config-sheriff/src/handpickedRules/sonarjsHandPickedRules.ts
@@ -3,4 +3,5 @@ import type { TSESLint } from '@typescript-eslint/utils';
 export const sonarjsHandPickedRules = {
   'sonarjs/cognitive-complexity': 0,
   'sonarjs/prefer-immediate-return': 0,
+  'sonarjs/no-duplicate-string': 0,
 } as const satisfies TSESLint.FlatConfig.Rules;
