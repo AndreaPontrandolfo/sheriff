77a324945c23ee09547022031b15c93bcdc43cf9
AndreaPontrandolfo <andrea.pontra@gmail.com>
2026-04-01 00:38:22 +0200

    refactor: update button component styles and variants
    - Introduced a default variant for buttons to enhance consistency.
    - Added new 'link' variant for improved text link styling.
    - Updated size variants to include default and large options for better flexibility.
    - Adjusted button layout to include gap for improved spacing between elements.

--- apps/docs-website/src/components/ui/button.tsx
+++ apps/docs-website/src/components/ui/button.tsx
@@ -3,27 +3,36 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 const variants = {
+  default:
+    'bg-fd-primary text-fd-primary-foreground shadow-xs hover:bg-fd-primary/90',
   primary:
     'bg-fd-primary text-fd-primary-foreground hover:bg-fd-primary/80 disabled:bg-fd-secondary disabled:text-fd-secondary-foreground',
   outline: 'border hover:bg-fd-accent hover:text-fd-accent-foreground',
   ghost: 'hover:bg-fd-accent hover:text-fd-accent-foreground',
   secondary:
     'border bg-fd-secondary text-fd-secondary-foreground hover:bg-fd-accent hover:text-fd-accent-foreground',
+  link: 'text-fd-primary underline-offset-4 hover:underline',
 } as const;
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md p-2 text-sm font-medium transition-colors duration-100 disabled:pointer-events-none disabled:opacity-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-fd-ring',
+  'inline-flex items-center justify-center gap-2 rounded-md p-2 text-sm font-medium transition-colors duration-100 disabled:pointer-events-none disabled:opacity-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-fd-ring',
   {
     variants: {
       variant: variants,
       // fumadocs use `color` instead of `variant`
       color: variants,
       size: {
+        default: 'h-9 px-4 py-2',
         sm: 'gap-1 px-2 py-1.5 text-xs',
+        lg: 'h-10 rounded-md px-6',
         icon: 'p-1.5 [&_svg]:size-5',
         'icon-sm': 'p-1.5 [&_svg]:size-4.5',
         'icon-xs': 'p-1 [&_svg]:size-4',
       },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
     },
   },
 );
